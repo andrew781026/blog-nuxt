c0222e66c15b60b43560d3b3a203e35e8661776f
王澍(Andrew) <andrew7810262000@gmail.com>
2020-10-16 16:48:48 +0800

    init nuxt.js project

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <ul>
+      <li class="m-12 bg-blue-200">
+        <NuxtLink to="/about">
+          關於我
+        </NuxtLink>
+      </li>
+      <li>
+        <a href="https://nuxtjs.org">其他外部連結</a>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+
+  export default {
+    name: "home"
+  }
+</script>
+
+<style>
+
+</style>
